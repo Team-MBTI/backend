74d92c898675c6c1a045c4fec801156446f6039c
Jimmy_승현수 <hsseung@maxst.com>
2023-06-28 22:21:52 +0900

    config setting

--- mbti_service/src/app.module.ts
+++ mbti_service/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MysqlModule } from './provider/database.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [MysqlModule],
+  imports: [
+    MysqlModule,
+    ConfigModule.forRoot({
+      envFilePath: [
+        process.env.NODE_ENV === 'main'
+          ? `${__dirname}/config/.main.env`
+          : `${__dirname}/config/.dev.env`,
+      ],
+      load: [databaseConfig],
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
