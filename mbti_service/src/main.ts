ca46c19402bb688f24a3ac24e2db998f18f8cbd8
sylee <sylee6529@gmail.com>
2023-06-29 15:52:46 +0900

    feat: swagger 설정

--- mbti_service/src/main.ts
+++ mbti_service/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MBTI Service API')
+    .setDescription('성격유형테스트 API 문서')
+    .setVersion('1.0.0')
+    .addTag('swagger')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   initializeTransactionalContext();
   await app.listen(3000);
 }
